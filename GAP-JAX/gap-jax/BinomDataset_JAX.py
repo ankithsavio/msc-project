500e3b5fa9c89aeff36ec30ab54ce451bfd1afa2
ankithsavio <ankithsavio@gmail.com>
2024-07-12 03:36:47 +0530

    implement gap with jax

--- GAP-JAX/gap-jax/BinomDataset_JAX.py
+++ GAP-JAX/gap-jax/BinomDataset_JAX.py
@@ -1,1 +1,8 @@
 import jax 
+import dm_pix ## image processing in JAX, has all operations we need
+from jax import make_jaxpr
+'''
+to check gpu 
+import jax.lib import xla_bridge
+print(xla_bridge.get_backend().platform)
+'''
